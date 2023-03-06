d6967c1a7e14050a924b746697eeefa133beddf0
uliakoroleva <L621I603@yandex.ru>
2023-03-06 12:52:04 +0300

    Добавили директорию настроек

--- test_simple.py
+++ test_simple.py
@@ -13,10 +13,10 @@ def open_browser():
 def test_one(open_browser):
     browser.open('https://google.com/ncr')
     browser.element('[name="q"]').should(be.blank).type('selene').press_enter()
-    browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
-
+    browser.element('[id="search"]').should(have.text('https://github.com › yashaka › selene'))
 
 def test_second(open_browser):
-    browser.open('https://google.com/ncr')
-    browser.element('[name="q"]').should(be.blank).type('Marg').press_enter()
-    browser.element('[id="search"]').should_not(have.text('Selene - User-oriented Web UI browser tests in Python'))
+    browser.open('https://google.com')
+    browser.element('[name="q"]').should(be.blank).type('gjklsfdhgjksdghdskj').press_enter()
+    browser.element('.card-section').should(have.text('По запросу gjklsfdhgjksdghdskj ничего не найдено.'))
+
